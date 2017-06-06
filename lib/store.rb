d3bceb926f00e9e889e44023abc11af8cc004c48
Arjun Lall <lallarjun@gmail.com>
2017-06-06 20:40:12 +0000

    stop destroy life cycle for condition

--- lib/store.rb
+++ lib/store.rb
@@ -3,10 +3,18 @@ class Store < ActiveRecord::Base
   validates :name, length: {minimum: 3}
   validates :annual_revenue, numericality: {greater_than: 0}
   validate :atleast_male_female
-
+  before_destroy :check_employees
   def atleast_male_female
     if (!mens_apparel) && (!womens_apparel)
       errors.add(:mens_apparel, "has to be atleast male or female")
     end
+  end
+
+  def check_employees
+    if employees.any?
+      errors.add(:employees, "cannot delete a store that has employyees")
+      false
+    end
+
   end
 end
