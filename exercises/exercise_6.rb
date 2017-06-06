43d255ce67838d37c862d01207427c3ef2fcc5e2
Arjun Lall <lallarjun@gmail.com>
2017-06-06 17:24:07 +0000

    linking tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,7 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Arjun", last_name: "Lall", hourly_rate: 9999999)
+@store1.employees.create(first_name: "Raymond", last_name: "Chow", hourly_rate: 0)
+
