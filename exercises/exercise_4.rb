008b38fcad66329bdfac8f33173f3dc8ea14b0d9
Arjun Lall <lallarjun@gmail.com>
2017-06-06 17:15:06 +0000

    where ruby on rails

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,25 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true)
+
+Store.create(name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false)
+
+Store.create(name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts @mens_stores.pluck(:name, :annual_revenue)
+
+# @mens_stores.each{|store| puts store.name, store.annual_revenue}
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+puts @womens_stores
